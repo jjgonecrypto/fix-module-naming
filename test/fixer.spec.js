d517f1e403108e37261ff0b4e7a1c43ed7ddb39e
Justin J. Moses <justinjmoses@gmail.com>
2014-10-16 15:59:50 -0400

    Moving cli and fixing tests

--- test/fixer.spec.js
+++ test/fixer.spec.js
@@ -13,21 +13,24 @@ describe('fixer', function () {
     beforeEach(function () {
         //make a copy of harness
         fs.copySync(src, dest);
-        fixer(dest);
     });
 
-    it('must rename files', function () {
-        var files = fs.readdirSync(dest);
-        expect(files).to.include('moduleA.js');
-        expect(files).to.include('ModuleB.js');
-        expect(files).to.not.include('module_a.js');
-        expect(files).to.not.include('module_b.js');
-        files = fs.readdirSync(path.join(dest, 'pk1'));
-        expect(files).to.include('moduleC.js');
-        expect(files).to.not.include('module_c.js');
-        files = fs.readdirSync(path.join(dest, 'pk2'));
-        expect(files).to.include('ModuleD.js');
-        expect(files).to.not.include('moduleD.js');
+    it('must rename files', function (done) {
+        fixer(dest, {}, function (err) {
+            expect(err).to.be.undefined;
+            var files = fs.readdirSync(dest);
+            expect(files).to.include('moduleA.js');
+            expect(files).to.include('ModuleB.js');
+            expect(files).to.not.include('module_a.js');
+            expect(files).to.not.include('module_b.js');
+            files = fs.readdirSync(path.join(dest, 'pk1'));
+            expect(files).to.include('moduleC.js');
+            expect(files).to.not.include('module_c.js');
+            files = fs.readdirSync(path.join(dest, 'pk2'));
+            expect(files).to.include('ModuleD.js');
+            expect(files).to.not.include('moduleD.js');
+            done();
+        });
     });
 
     function getContents(oldPath, newPath) {
@@ -38,22 +41,26 @@ describe('fixer', function () {
     }
 
     it('must update module definitions', function (done) {
-        var moduleA = getContents('module_a.js', 'moduleA.js');
-        expect(moduleA.oldContents).to.equal(moduleA.newContents);
+        fixer(dest, {}, function (err) {
+            expect(err).to.be.undefined;
 
-        var moduleB = getContents('module_b.js', 'ModuleB.js');
-        expect(moduleB.newContents).to.equal(moduleB.oldContents.replace('./module_a', './moduleA').replace('./pk2/moduleD', './pk2/ModuleD'));
+            var moduleA = getContents('module_a.js', 'moduleA.js');
+            expect(moduleA.oldContents).to.equal(moduleA.newContents);
 
-        var moduleC = getContents('pk1/module_c.js', 'pk1/ModuleC.js');
-        expect(moduleC.newContents).to.equal(moduleC.oldContents.replace('../module_b', '../ModuleB'));
+            var moduleB = getContents('module_b.js', 'ModuleB.js');
+            expect(moduleB.newContents).to.equal(moduleB.oldContents.replace('./module_a', './moduleA').replace('./pk2/moduleD', './pk2/ModuleD'));
 
-        var moduleD = getContents('pk2/moduleD.js', 'pk2/ModuleD.js');
-        expect(moduleD.newContents).to.equal(moduleD.oldContents);
+            var moduleC = getContents('pk1/module_c.js', 'pk1/ModuleC.js');
+            expect(moduleC.newContents).to.equal(moduleC.oldContents.replace('../module_b', '../ModuleB'));
 
-        //ensure all module requires are case-sensitive
-        detect(dest, function (invalid) {
-            expect(invalid).to.be.empty;
-            done();
+            var moduleD = getContents('pk2/moduleD.js', 'pk2/ModuleD.js');
+            expect(moduleD.newContents).to.equal(moduleD.oldContents);
+
+            //ensure all module requires are case-sensitive
+            detect(dest, function (invalid) {
+                expect(invalid).to.be.empty;
+                done();
+            });
         });
     });
 
